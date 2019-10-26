32460cc5f2f5c66b73772316f8b3fb9765dc46dc
mark-quinn <markquinn97@hotmail.com>
2019-10-26 19:46:03 +0100

    configure base root and webpacker #purebeltin

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   resources :tags
   resources :descriptions
   resources :exhibits
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'pages#index', as: 'index'
 end
