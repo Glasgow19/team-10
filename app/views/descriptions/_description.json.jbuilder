73c4330413e5f6162ea9c1ca869897751f54caa4
dsewerynski <dsewerynski@dundee.ac.uk>
2019-10-26 16:56:31 +0100

    adding description model

---
+++ app/views/descriptions/_description.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! description, :id, :text, :exhibit_id, :created_at, :updated_at
+json.url description_url(description, format: :json)
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_26_153932) do
+ActiveRecord::Schema.define(version: 2019_10_26_155334) do
 
-  create_table "exhibts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "descriptions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.text "text"
+    t.bigint "exhibit_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["exhibit_id"], name: "index_descriptions_on_exhibit_id"
+  end
+
+  create_table "exhibits", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "descriptions", "exhibits"
 end
