b52442a228cb59438cde9b65a420098b1698eb68
Luke Poutney <2314348p@student.gla.ac.uk>
2019-10-27 05:39:37 +0000

    query complete

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,4 +9,8 @@ class PagesController < ApplicationController
   def zones_view
     @exhibits = Zone.zone_exhibits params[:id]
   end
+
+  def exhibits_view 
+      @tags = Exhibit.exhibit_tags params[:id]
+  end
 end
