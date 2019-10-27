d9c5a16f8f5bbf73ef5641ec76b83acb61e2f77c
Jacob <jacosdin9@gmail.com>
2019-10-27 01:52:49 +0100

    populated floors and zones

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#Floors
+lvl1 = Floor.new(level: 0).save!
+lvl2 = Floor.new(level: 1).save!
+lvl3 = Floor.new(level: 2).save!
+
+#Zones
+zoneA = Zone.new(name: "A", floor_id: 1).save!
+zoneB = Zone.new(name: "B", floor_id: 1).save!
+zoneC = Zone.new(name: "C", floor_id: 1).save!
+zoneD = Zone.new(name: "D", floor_id: 2).save!
+zoneE = Zone.new(name: "E", floor_id: 2).save!
+zoneF = Zone.new(name: "F", floor_id: 2).save!
+zoneG = Zone.new(name: "G", floor_id: 3).save!
+zoneH = Zone.new(name: "H", floor_id: 3).save!
+zoneI = Zone.new(name: "I", floor_id: 3).save!
